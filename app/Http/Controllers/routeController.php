dbec1187b377cb388ac9bef7adc00aeac2ba41ff
cosiitoypg <79027821+cosiitoypg@users.noreply.github.com>
2021-04-25 19:36:05 -0500

    actualizacion inicial
    actualizacion inicial

---
+++ app/Http/Controllers/routeController.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\db_credit;
+use App\db_not_pay;
+use App\db_summary;
+use App\db_blacklists;
+use App\db_supervisor_has_agent;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class routeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(function ($request, $next) {
+            $this->id = Auth::user()->id;
+            if (!db_supervisor_has_agent::where('id_user_agent', Auth::id())->exists()) {
+                die('No existe relacion Usuario y Agente');
+            }
+            return $next($request);
+        });
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = db_credit::where('credit.id_agent', Auth::id())
+            ->join('users','credit.id_user','=','users.id')
+            ->where('credit.status', 'inprogress')
+            ->select('credit.*')
+            ->orderBy('credit.order_list', 'asc')
+            ->get();
+        $data_filter = array();
+        $dt = Carbon::now();
+
+        foreach ($data as $k => $d) {
+
+
+            $tmp_amount = db_summary::where('id_credit', $d->id)
+            ->where('id_agent', Auth::id())
+            ->sum('amount');
+            $amount_total = ($d->amount_neto) + ($d->amount_neto * $d->utility);
+            $tmp_quote = round(floatval(($amount_total / $d->payment_number)), 2);
+            $tmp_rest = round(floatval($amount_total - $tmp_amount), 2);
+
+            $d->positive = $tmp_amount;
+            $d->payment_quote = $tmp_quote;
+            $d->rest = round(floatval($amount_total - $tmp_amount), 2);            
+            $d->payment_done = db_summary::where('id_credit', $d->id)->count();
+            $d->user = User::find($d->id_user);
+            $d->amount_total = $amount_total;
+            $d->days_rest = $dt->diffInDays(Carbon::parse($d->created_at));
+            $d->saldo = $d->amount_total - (db_summary::where('id_credit', $d->id)->sum('amount'));
+            $d->quote = (floatval($d->amount_neto * $d->utility) + floatval($d->amount_neto)) / floatval($d->payment_number);
+            $d->setAttribute('last_pay', db_summary::where('id_credit', $d->id)->orderBy('id', 'desc')->first());
+
+            if (!db_summary::where('id_credit', $d->id)->whereDate('created_at', '=', Carbon::now()->toDateString())->exists()) {
+                $findExist = !db_not_pay::whereDate('created_at', '=', Carbon::now()->toDateString())->where('id_credit', $d->id)->exists();
+                $findPending = !db_blacklists::where('id_credit', $d->id)->exists();
+                if ($findExist && $findPending) {
+                    $data_filter[] = $d;
+                }
+
+            }
+
+
+        }
+
+        $data_all = array(
+            'clients' => $data_filter
+        );
+
+        return view('route.index', $data_all);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Request $request, $id)
+    {
+        $id_credit = $request->id_credit;
+        $direction = $request->direction;
+
+        if (!isset($direction)) {
+            return 'Direction';
+        };
+        if ($direction == 'up') {
+            $direction = '<';
+            $order = 'DESC';
+        };
+        if ($direction == 'down') {
+            $direction = '>';
+            $order = 'ASC';
+        };
+
+
+        $data = db_credit::where('id_agent', Auth::id())
+            ->orderBy('order_list', $order)
+            ->where('order_list', $direction, $id)
+            ->where('status', 'inprogress')
+            ->first();
+
+        $no_pay = db_not_pay::whereDate('created_at', Carbon::now()->toDateString())
+            ->where('id_credit', $data->id)
+            ->exists();
+
+        db_credit::where('id', $id_credit)->update([
+            'order_list' => ($data->order_list)
+        ]);
+
+        db_credit::where('id', $data->id)->update([
+            'order_list' => ($id)
+        ]);
+
+        if ($no_pay) {
+            return redirect('/route?hide=true');
+        } else {
+            return redirect('/route');
+        }
+
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('/home');
+});
+
+Route::get('/logout', 'Auth\LoginController@logout');
+Route::get('/cron', 'closeController@close_automatic');
+
+Auth::routes();
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('client', 'userController', ['only' => ['create', 'show']])->middleware('close');
+    Route::resource('client', 'userController', ['except' => ['create', 'show']]);
+    Route::resource('payment', 'paymentController')->middleware('close');
+    Route::get('payment-export', 'paymentController@export')->middleware('auth');
+    Route::resource('cartera', 'carteraController')->middleware('close');
+    Route::resource('summary', 'summaryController')->middleware('close');
+    Route::resource('simulator', 'simulatorController');
+    Route::resource('route', 'routeController')->middleware('close');
+    Route::resource('statistics', 'statisticsUserController');
+    Route::resource('blacklists', 'blacklistsController');
+    Route::resource('history', 'historyController');
+    Route::resource('transaction', 'transactionController');
+    Route::resource('bill', 'billController')->middleware('close');
+    Route::resource('not-pay', 'NotPaymentController')->middleware('auth');
+    Route::get('export', 'NotPaymentController@export')->middleware('auth');
+});
+
+
+
+Route::prefix('supervisor')->group(function () {
+    Route::resource('agent', 'agentController');
+    Route::resource('close', 'closeController');
+    Route::resource('client', 'clientController');
+    Route::resource('tracker', 'trackerController');
+    Route::resource('review', 'reviewController');
+    Route::resource('statistics', 'statisticsController');
+    Route::resource('cash', 'cashController');
+    Route::resource('bill', 'billsupervisorController');
+    Route::resource('credit', 'creditController');
+    Route::resource('summary', 'supervisorSummaryController');
+
+    /*-----Sub Menu-----*/
+    Route::prefix('menu')->middleware(['auth'])->group(function () {
+        Route::resource('history', 'subHistoryController');
+        Route::resource('transitions', 'subTransitionsController');
+        Route::resource('route', 'subRouteController');
+        Route::resource('bill', 'subBillController');
+        Route::resource('close', 'subCloseController');
+        Route::resource('edit', 'subEditController');
+        Route::resource('report', 'subReportController');
+        Route::resource('done', 'subDoneController');
+    });
+});
+
+Route::prefix('admin')->middleware(['auth'])->group(function () {
+    Route::resource('session', 'sessionController')->only([
+        'store'
+    ]);
+});
+
+
+Route::prefix('admin')->middleware(['admin'])->group(function () {
+    Route::resource('user', 'adminUserController');
+    Route::resource('session', 'sessionController')->only([
+        'update'
+    ]);
+    Route::resource('route', 'adminRouteController');
+});
